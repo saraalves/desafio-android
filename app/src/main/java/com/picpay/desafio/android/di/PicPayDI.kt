1953f059027b59657b54487541fed8e22f345f97
Sara Alves <saraalves14@hotmail.com>
2021-04-09 11:55:55 -0300

    limpando o código

--- app/src/main/java/com/picpay/desafio/android/di/PicPayDI.kt
+++ app/src/main/java/com/picpay/desafio/android/di/PicPayDI.kt
@@ -13,25 +13,20 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-// timeout pra chamdada
+
 const val TIMEOUT_5_SECONDS = 5L
-//sempre criar a base url até o primeiro /
 const val BASE_URL = "http://careers.picpay.com/"
 
-//
 val viewModelModules = module {
     viewModel { PicPayViewModel(get(), get()) }
 }
 
-
-//iniciando a instancia do Room, pra manter ele de pé
 val storageModules = module {
     single { Room.databaseBuilder(
         get(),
         AppDatabase::class.java,
         "userdata")
         .build()}
-    // esse outro é só pra criar uma instancia dele aqui dentro do UserDAO
     single { get<AppDatabase>().userDAO() }
 }
 
@@ -39,7 +34,7 @@ val repositoryModules = module {
     single{  PicPayRepository(get()) }
 }
 
-// inciando a instancia do retrofit uma vez só pq é um single
+// inciando a instancia do retrofit
 val serviceModules = module {
     single {
         createWebService(
@@ -55,14 +50,13 @@ val serviceModules = module {
     }
 }
 
-// colocando pra acompanhar as chamdas que eu faço do retrofit, intecpeta a chamda do backend exibindo o que retorna de request e response
 private fun configDebugLog(): HttpLoggingInterceptor {
     val log = HttpLoggingInterceptor()
     log.level = HttpLoggingInterceptor.Level.BODY
     return log
 }
 
-// só funciona pra criar a instancia do retorfit
+// criar a instancia do retrofit
 fun createWebService(
     okHttpClient: OkHttpClient, baseUrl: String
 ): PicPayService {
